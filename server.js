4ab2d210ccdf0c9725fa846393df30028016a2d1
sriram84502 <184502@sasi.edu.in>
2025-05-18 20:06:47 +0530

    save

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['https://manscara.lovabel.com','https://preview--mascara-admin-console-54.lovable.app','https://manscara-glow-landing.lovable.app','https://preview--manscara-glow-landing.lovable.app','http://localhost:8080','https://preview--mascara-admin-console.lovable.app','https://v0-admin-panel-with-user-insights.vercel.app','https://manscara-admin-control-hub.lovable.app'],
+  origin: ['https://manscara-glow-landing.onrender.com','https://preview--mascara-admin-console-54.lovable.app','https://manscara-glow-landing.lovable.app','https://preview--manscara-glow-landing.lovable.app','http://localhost:8080','https://preview--mascara-admin-console.lovable.app','https://v0-admin-panel-with-user-insights.vercel.app','https://manscara-admin-control-hub.lovable.app'],
   credentials: true
 }));
 const helmet = require('helmet');
