955ebce9818b2f475fc6bb7ddd9a2a2b74d0835e
Sriram <115489336+sriram84502@users.noreply.github.com>
2025-07-07 12:52:44 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 
 // Utility to generate JWT token
 const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET);
 };
 
 // @desc    Register a new user
